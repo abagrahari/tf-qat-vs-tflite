8ad7d6f2cd5ced03152e5d671dc7f70cca807ff1
Abhinav Agrahari <abhinav.agrahari@appliedbrainresearch.com>
2021-06-16 15:39:53 -0400

    Get raw outputs from tflite Interpreter

--- main.py
+++ main.py
@@ -7,17 +7,22 @@
 # - Use the model to create an actually quantized model for the TFLite backend.
 # - See the persistence of accuracy in TFLite and a 4x smaller model.
 
+# The aim is to check for equivalence between a QAT model and
+# its equivalent TFLite model running on a PC.
+
 import os
-import tempfile
+import warnings
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+MODEL_TYPE = "CNN"
 
 import numpy as np
 import tensorflow as tf
 import tensorflow_model_optimization as tfmot
 
 from tensorflow import keras
-from pprint import pprint
+
+tf.random.set_seed(4)
 
 # Train a model for MNIST without quantization aware training
 
@@ -30,16 +35,30 @@ train_images = train_images / 255.0
 test_images = test_images / 255.0
 
 # Define the model architecture.
-model = keras.Sequential(
-    [
-        keras.layers.InputLayer(input_shape=(28, 28)),
-        keras.layers.Reshape(target_shape=(28, 28, 1)),
-        keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation="relu"),
-        keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        keras.layers.Flatten(),
-        keras.layers.Dense(10),
-    ]
-)
+def get_model(model_type: str):
+    """Options: "dense#', or 'cnn'."""
+    layers = [keras.layers.InputLayer(input_shape=(28, 28))]
+    if "dense" in model_type:
+        # Model is n stacked Dense layers
+        layers.append(keras.layers.Flatten())
+        layers.extend(
+            keras.layers.Dense(10) for _ in range(int(model_type.split("dense")[1]))
+        )
+    else:
+        # Model is a CNN
+        layers.extend(
+            [
+                keras.layers.Reshape(target_shape=(28, 28, 1)),
+                keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation="relu"),
+                keras.layers.MaxPooling2D(pool_size=(2, 2)),
+                keras.layers.Flatten(),
+                keras.layers.Dense(10),
+            ]
+        )
+    return keras.Sequential(layers)
+
+
+model = get_model(MODEL_TYPE)
 
 # Train the digit classification model
 model.compile(
@@ -64,17 +83,17 @@ model.fit(
 
 quantize_model = tfmot.quantization.keras.quantize_model
 
-# q_aware stands for for quantization aware.
-q_aware_model = quantize_model(model)
+# quantization aware model
+qat_model = quantize_model(model)
 
 # `quantize_model` requires a recompile.
-q_aware_model.compile(
+qat_model.compile(
     optimizer="adam",
     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
     metrics=["accuracy"],
 )
 
-q_aware_model.summary()
+qat_model.summary()
 
 # Train and evaluate the model against baseline
 # To demonstrate fine tuning after training the model for just an epoch, fine tune with quantization aware training on a subset of the training data.
@@ -82,7 +101,7 @@ q_aware_model.summary()
 train_images_subset = train_images[0:1000]  # out of 60000
 train_labels_subset = train_labels[0:1000]
 
-q_aware_model.fit(
+qat_model.fit(
     train_images_subset,
     train_labels_subset,
     batch_size=500,
@@ -90,36 +109,62 @@ q_aware_model.fit(
     validation_split=0.1,
 )
 
-# For this example, there is minimal to no loss in test accuracy after quantization aware training, compared to the baseline.
+# there is minimal to no loss in test accuracy after quantization aware training, compared to the baseline.
 _, baseline_model_accuracy = model.evaluate(test_images, test_labels, verbose=0)
-
-_, q_aware_model_accuracy = q_aware_model.evaluate(test_images, test_labels, verbose=0)
-
+_, q_aware_model_accuracy = qat_model.evaluate(test_images, test_labels, verbose=0)
 print("Baseline test accuracy:", baseline_model_accuracy)
 print("Quant test accuracy:", q_aware_model_accuracy)
 
-# Create quantized model for TFLite backend
-# After this, you have an actually quantized model with int8 weights and uint8 activations.
-
-converter = tf.lite.TFLiteConverter.from_keras_model(q_aware_model)
+# Create quantized model for TFLite backend, using 'Dynamic range quantization'
+# After this, we have an actually quantized model with int8 weights and uint8 activations.
+converter = tf.lite.TFLiteConverter.from_keras_model(qat_model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
+# TF's QAT example uses Dynamic range quantization
 
+# Converter options for all INT8 conversion:
+# TODO use representive_dataset (full integer quantization)
+# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# converter.inference_input_type = tf.int8  # or tf.uint8
+# converter.inference_output_type = tf.int8  # or tf.uint8
+# converter.representative_dataset =representative_dataset
 quantized_tflite_model = converter.convert()
 
-# See persistence of accuracy from TF to TFLite
-# Define a helper function to evaluate the TF Lite model on the test dataset.
+# Evaluate the quantized model and see that the accuracy from TensorFlow persists to the TFLite backend.
+interpreter = tf.lite.Interpreter(model_content=quantized_tflite_model)
+interpreter.allocate_tensors()
+
+
+def tflite_predict(interpreter: tf.lite.Interpreter):
+    """Helper function to return outputs on the test dataset using the TF Lite model."""
+    # https://www.tensorflow.org/lite/guide/inference#load_and_run_a_model_in_python
+    input_index = interpreter.get_input_details()[0]["index"]
+    output_index = interpreter.get_output_details()[0]["index"]
+
+    # Run predictions on every image in the "test" dataset.
+    outputs = []
+    for test_image in test_images:
+        # Pre-processing: add batch dimension and convert to float32 to match with
+        # the model's input data format.
+        test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
+        interpreter.set_tensor(input_index, test_image)
+
+        # Run inference.
+        interpreter.invoke()
+
+        # Post-processing: remove batch dimension
+        outputs.append(interpreter.get_tensor(output_index)[0])
+
+    return np.array(outputs)
 
 
 def evaluate_model(interpreter):
+    """Helper function to evaluate the TF Lite model on the test dataset."""
     input_index = interpreter.get_input_details()[0]["index"]
     output_index = interpreter.get_output_details()[0]["index"]
-    pprint(interpreter.get_input_details())
-    pprint(interpreter.get_output_details())
-    print()
 
     # Run predictions on every image in the "test" dataset.
     prediction_digits = []
-    for i, test_image in enumerate(test_images):
+    for test_image in test_images:
         # Pre-processing: add batch dimension and convert to float32 to match with
         # the model's input data format.
         test_image = np.expand_dims(test_image, axis=0).astype(np.float32)
@@ -140,44 +185,46 @@ def evaluate_model(interpreter):
     return accuracy
 
 
-# You evaluate the quantized model and see that the accuracy from TensorFlow persists to the TFLite backend.
-interpreter = tf.lite.Interpreter(model_content=quantized_tflite_model)
-interpreter.allocate_tensors()
-
 test_accuracy = evaluate_model(interpreter)
-
 print("Quant TF test accuracy:", q_aware_model_accuracy)
 print("Quant TFLite test_accuracy:", test_accuracy)
-# See 4x smaller model from quantization
-# You create a float TFLite model and then see that the quantized TFLite model is 4x smaller.
-
-# Create float TFLite model.
-float_converter = tf.lite.TFLiteConverter.from_keras_model(model)
-float_tflite_model = float_converter.convert()
-
-# Measure sizes of models.
-_, float_file = tempfile.mkstemp(".tflite")
-_, quant_file = tempfile.mkstemp(".tflite")
-
-with open(quant_file, "wb") as f:
-    f.write(quantized_tflite_model)
-
-with open(float_file, "wb") as f:
-    f.write(float_tflite_model)
-
-print("Float model in Mb:", os.path.getsize(float_file) / float(2 ** 20))
-print("Quantized model in Mb:", os.path.getsize(quant_file) / float(2 ** 20))
 
 # Run on QAT model
-qat_output = q_aware_model.predict(test_images)
-# Run on simulated device
-# host_sim_output = host_sim.run(inputs)
+qat_output = qat_model.predict(test_images)
+# Run on tflite
+tflite_output = tflite_predict(interpreter)
 
 qat_output = qat_output.flatten()
-# host_sim_output = host_sim_output.flatten()
-print(qat_output)
+tflite_output = tflite_output.flatten()
+
+
+def output_stats(x, y, model: str, test_status: bool, tol: float):
+    """Output summary statistics"""
+    err = np.abs(x - y)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        err_rel = err / np.abs(y)
+        # Ignore "divide by zero" RuntimeWarning
+    # Filter out nan and inf created by dividing by 0
+    err_rel = err_rel[np.isfinite(err_rel)]
+
+    status = "Passed" if test_status else "Failed"
+    print(f"--------------------- RESULTS ---------------------")
+    print(f"Model: {model}; TestStatus: {status}")
+    print(f"Max Error: {np.max(err)}")
+    print(f"Max Relative Error: {np.max(err_rel)}")
+    print(f"Mean Error: {np.mean(err)}")
+    print("-------------------------------------------------")
+
+
+outputs_close = np.allclose(qat_output, tflite_output, rtol=0, atol=1e-2)
+output_stats(
+    qat_output,
+    tflite_output,
+    MODEL_TYPE,
+    outputs_close,
+    1e-2,
+)
 
 # Conclusion
 # In this tutorial, you saw how to create quantization aware models with the TensorFlow Model Optimization Toolkit API and then quantized models for the TFLite backend.
-
-# You saw a 4x model size compression benefit for a model for MNIST, with minimal accuracy difference.
