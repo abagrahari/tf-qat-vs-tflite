Add explanation for wrong domain calculation
My calculation was incorrectly converting between input and output
quantization domains. Formula is incorrect and will not be fixed. Have
refactored to a separate calculation mode.

compare_dense.py
@@ -132,24 +132,20 @@ for regular_layer, custom_layer in zip(base_model.layers, custom_model.layers):
 
 _, base_model_accuracy = base_model.evaluate(test_images, test_labels, verbose=0)
 _, custom_model_accuracy = custom_model.evaluate(test_images, test_labels, verbose=0)
-
-
-# Evaluate and see if accuracy from TensorFlow persists to TFLite.
 tflite_model_accuracy = tflite_runner.evaluate_tflite_model(
     tflite_model, test_images, test_labels
 )
 
-print()
 print("Base test accuracy:", base_model_accuracy)
 print("Custom test accuracy:", custom_model_accuracy)
 print("TFLite test_accuracy:", tflite_model_accuracy)
 
-# Run test dataset on custom, and TFLite models
+# Run test dataset on models
 base_output: np.ndarray = base_model.predict(test_images)
-base_output = base_output.flatten()
 custom_output: np.ndarray = custom_model.predict(test_images)
-custom_output = custom_output.flatten()
 tflite_output = tflite_runner.run_tflite_model(tflite_model, test_images)
+base_output = base_output.flatten()
+custom_output = custom_output.flatten()
 tflite_output = tflite_output.flatten()
 
 # Check that Custom model is closer to tflite, than base model

custom_layers.py
@@ -246,7 +246,7 @@ class DenseTFLite(Dense):
         self.bias_zp = bias_zp
         self.output_scale = output_scale
         self.output_zp = output_zp
-        self.mode = "gary"  # For debugging: either "dequant_before", "mine", or "gary"
+        self.mode = "dequant_before"  # For debugging: either "dequant_before", "wrong_domain", "gary", or "mine"
 
     def call(self, inputs: tf.Tensor):
 
@@ -276,9 +276,26 @@ class DenseTFLite(Dense):
             y = tf.nn.bias_add(y, dequant_bias)
             return y
 
+        if self.mode == "wrong_domain":
+            # This approach will not work, since it would be incorrectly converting between
+            # input and ouput quantization domains.
+            # quant_input and quant_kernel are not enough to know the quantization domains
+            # We must also account for scale_i, scale_kernel, zp_i, zp_kernel to
+            # convert between domains
+
+            # Use regular matmul and addition
+            y: tf.Tensor = tf.matmul(
+                tf.cast(quant_input, tf.int32), tf.cast(quant_kernel, tf.int32)
+            )
+            y = tf.nn.bias_add(y, quant_bias)
+            # Outputs will have int32 type.
+            # Dequantize outputs
+            y = dequant_from_tflite_params(y, self.output_scale, self.output_zp)
+            return y
+
         if self.mode == "gary":
             # Formula per Gary's method - essentially just: dequant(inputs_int) * dequant(kernel_int) + dequant(bias_int)
-            # i.e. exact same as dequantizing all the values above, before using them
+            # i.e. exact same as dequantizing all the values above, before using them (mode="dequant_before")
             # It also gives the exact same output
             y = (
                 tf.matmul(
@@ -288,16 +305,4 @@ class DenseTFLite(Dense):
                 * self.input_scale
                 * self.kernel_scale
             ) + dequant_from_tflite_params(quant_bias, self.bias_scale, self.bias_zp)
-            return y
-
-        if self.mode == "mine":
-            # My attempt. This doesn't seem to work.
-            # Use regular matmul and addition
-            y: tf.Tensor = tf.matmul(
-                tf.cast(quant_input, tf.int32), tf.cast(quant_kernel, tf.int32)
-            )
-            y = tf.nn.bias_add(y, quant_bias)
-            # Outputs will have int32 type.
-            # Dequantize outputs
-            y = dequant_from_tflite_params(y, self.output_scale, self.output_zp)
             return y