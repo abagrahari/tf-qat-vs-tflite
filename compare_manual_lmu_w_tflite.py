254de12a35343a7fe19ada71af840fc2b2d7ece9
Abhinav Agrahari <abhinav.agrahari@appliedbrainresearch.com>
2021-08-05 11:48:06 -0400

    temp cleanup

--- compare_manual_lmu_w_tflite.py
+++ compare_manual_lmu_w_tflite.py
@@ -3,8 +3,6 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import numpy as np
 import tensorflow as tf
-import utils
-from custom_layers import calculate_min_max_from_tflite, calculate_scale_zp_from_min_max
 
 import keras_lmu
 
@@ -12,21 +10,6 @@ import keras_lmu
 tf.random.set_seed(3)
 rng = np.random.RandomState(3)
 w = rng.uniform(-1, 1, size=(10, 10))
-
-
-def adjust_params(min, max):
-    """Adjust quantization parameters as tflite does"""
-    return [
-        t.numpy()
-        for t in calculate_min_max_from_tflite(*calculate_scale_zp_from_min_max(min, max))
-    ]
-
-
-##################################################
-# Manual computation - tflite
-##################################################
-
-# TODO: adapt from compare_manual_w_tflite.py
 
 ##################################################
 # tflite computation
@@ -60,7 +43,7 @@ model = tf.keras.Sequential(
     ]
 )
 model.build(inputs.shape)
-manual_output = model(inputs)
+model_output = model(inputs)
 
 
 def representative_dataset():
@@ -78,25 +61,26 @@ converter.optimizations = [tf.lite.Optimize.DEFAULT]
 tflite_model = converter.convert()
 with open("saved_models/lmu.tflite", "wb") as f:
     f.write(tflite_model)
+
+print("---------Converted to tflite!---------")
+
 interpreter = tf.lite.Interpreter(model_content=tflite_model)
 interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()[0]
 output_details = interpreter.get_output_details()[0]
 
+print("---------Allocated tensors---------")
+
 tflite_output = []
 for input in inputs:
-    # input_scale, input_zero_point = input_details["quantization"]
-    # input = np.round(input / input_scale + input_zero_point).astype(np.uint8)
     input = np.expand_dims(input, axis=0).astype(input_details["dtype"])
     interpreter.set_tensor(input_details["index"], input)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details["index"])[0]
-    # output_scale, output_zero_point = output_details["quantization"]
-    # output = (output.astype(np.float32) - output_zero_point) * output_scale
     tflite_output.append(output)
 tflite_output = np.array(tflite_output)
 
 # Compare outputs
-manual_output = np.array(manual_output).flatten()
+model_output = np.array(model_output).flatten()
 tflite_output = np.array(tflite_output).flatten()
-utils.output_stats(manual_output, tflite_output, "Manual LMU vs tflite", 1e-2, 0)
+print("Done.")
