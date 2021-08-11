a82aea3f47a6a6963af0c190b8a63997c48f3cb1
Abhinav Agrahari <abhinav.agrahari@appliedbrainresearch.com>
2021-08-11 12:26:20 -0400

    Compare manual lmu to fp32 tflite

--- compare_manual_lmu_w_tflite.py
+++ compare_manual_lmu_w_tflite.py
@@ -35,37 +35,45 @@ dense_kernel = rng.uniform(-1, 1, size=(10, 10))
 
 ##################################################
 # Manual computation - mimicking tflite
+# Using settings from tflite model to recreate tflite model
 ##################################################
-# As per TfLite's QuantizeModel https://git.io/J4hxt, it seems that a full fp32 forward pass is done
-# after which, quantization parameters are independantly calculated. Then, the model is 'quantized'
+# Similar approach to compare_manual_w_tflite.py
 
 # Run fp32 forward pass
-# TODO run on all 320 inputs
-strided_slice_output = tf.strided_slice(
-    inputs[[0]],
-    # Settings from tflite model
-    begin=[0, 0, 0],
-    end=[0, 1, 4],
-    strides=[1, 1, 1],
-    begin_mask=5,
-    ellipsis_mask=0,
-    end_mask=5,
-    new_axis_mask=0,
-    shrink_axis_mask=2,
-).numpy()
-# Concat op
-manual_output = tf.concat([strided_slice_output, tf.zeros((1, 10))], axis=1)
-# FC1 (lmu_kernel matches quantized wieghts in tflite)
-manual_output = tf.matmul(manual_output, lmu_kernel)
-# FC2
-manual_output = tf.matmul(manual_output, lmu_recurrent)
-# Concat op
-manual_output = tf.concat([strided_slice_output, manual_output], axis=1)
-# FC (relu)
-manual_output = tf.matmul(manual_output, hidden_kernel)
-manual_output = tf.nn.relu(manual_output)
-# FC (Dense layer)
-manual_output = tf.matmul(manual_output, dense_kernel)
+manual_outputs = []
+
+for input in inputs:
+    # run on all inputs
+    input = np.expand_dims(input, axis=0)
+    strided_slice_output = tf.strided_slice(
+        input,
+        # Settings from tflite model
+        begin=[0, 0, 0],
+        end=[0, 1, 4],
+        strides=[1, 1, 1],
+        begin_mask=5,
+        ellipsis_mask=0,
+        end_mask=5,
+        new_axis_mask=0,
+        shrink_axis_mask=2,
+    ).numpy()
+    # Concat op
+    manual_output = tf.concat([strided_slice_output, tf.zeros((1, 10))], axis=1)
+    # FC1 (lmu_kernel matches wieghts in tflite)
+    manual_output = tf.matmul(manual_output, lmu_kernel)
+    # FC2 #TODO: lmu_recurrent does not match weights in tflite
+    # TODO: where are the weights in tflite from??
+    manual_output = tf.matmul(manual_output, lmu_recurrent)
+    # Concat op
+    manual_output = tf.concat([strided_slice_output, manual_output], axis=1)
+    # FC (relu) (hidden_kernel matches weights in tflite)
+    manual_output = tf.matmul(manual_output, hidden_kernel)
+    manual_output = tf.nn.relu(manual_output)
+    # FC (Dense layer) (dense_kernel matches weights in tflite)
+    manual_output = tf.matmul(manual_output, dense_kernel)
+    manual_outputs.append(manual_output)
+
+# TODO: quantized forward pass
 
 ##################################################
 # tflite computation
@@ -109,10 +117,11 @@ def representative_dataset():
 
 converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-converter.inference_input_type = tf.uint8
-converter.inference_output_type = tf.uint8
-converter.representative_dataset = representative_dataset
+# TODO: Temporarily comment out quantizaiton
+# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+# converter.inference_input_type = tf.uint8
+# converter.inference_output_type = tf.uint8
+# converter.representative_dataset = representative_dataset
 
 tflite_model = converter.convert()
 
@@ -126,22 +135,21 @@ output_details = interpreter.get_output_details()[0]
 
 tflite_output = []
 for input in inputs:
-    input_scale, input_zero_point = input_details["quantization"]
-    input = np.round(input / input_scale + input_zero_point).astype(np.uint8)
+    # input_scale, input_zero_point = input_details["quantization"]
+    # input = np.round(input / input_scale + input_zero_point).astype(np.uint8)
     input = np.expand_dims(input, axis=0).astype(input_details["dtype"])
     interpreter.set_tensor(input_details["index"], input)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details["index"])[0]
-    output_scale, output_zero_point = output_details["quantization"]
-    output = (output.astype(np.float32) - output_zero_point) * output_scale
+    # output_scale, output_zero_point = output_details["quantization"]
+    # output = (output.astype(np.float32) - output_zero_point) * output_scale
     tflite_output.append(output)
 tflite_output = np.array(tflite_output)
 
 # Compare outputs
-manual_output = np.array(manual_output).flatten()
+manual_outputs = np.array(manual_outputs).flatten()
 model_output = np.array(model_output).flatten()
 tflite_output = np.array(tflite_output).flatten()
 utils.output_stats(model_output, tflite_output, "Keras model vs tflite", 1e-2, 0)
-print(manual_output.shape, model_output.shape)
-utils.output_stats(manual_output, model_output[:10], "Manual LMU vs Keras Model (10)", 1e-2, 0)
-utils.output_stats(manual_output, tflite_output[:10], "Manual LMU vs tflite (10)", 1e-2, 0)
+utils.output_stats(manual_outputs, model_output, "Manual LMU vs Keras Model", 1e-2, 0)
+utils.output_stats(manual_outputs, tflite_output, "Manual LMU vs tflite", 1e-2, 0)
